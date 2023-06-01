5f91dbf805cd2675375f0835e1e1d7e3f4c88df1
JoseAFlores777 <joseadolfoizaguirreflores@gmail.com>
2023-06-01 17:15:32 -0600

    social Login impl
    https://www.youtube.com/watch?v=c_-b_isI4vg&t=8884s

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -3,14 +3,21 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+  src
+}) => {
+  console.log(src);
   return (
     <Image
       className="rounded-full"
       height="30"
       width="30"
       alt="Avatar"
-      src="/images/placeholder.jpg"
+      src={src || '/images/placeholder.jpg'}
     />
   )
 }
